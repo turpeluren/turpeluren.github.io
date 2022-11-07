5c5b1cc3e1854aee2041a4063c73653fb0244149
turpeluren <thenotteam@gmail.com>
2022-11-07 19:57:45 +0000

    two sidebar-buttons mobile

--- js/sidebar.js
+++ js/sidebar.js
@@ -5,6 +5,7 @@
     
 
     let portfoliocollapse = document.getElementsByClassName('portfoliocollapse')[0];
+    let portfoliocollapseRight = document.getElementsByClassName('portfoliocollapse')[1];
     let portfolionav = document.getElementsByClassName('portfolionavbar')[0];
     let sidebar = document.getElementsByClassName('sidebar')[0];
     let topnavLinks = document.getElementsByClassName('topnavLinks')[0];
@@ -13,34 +14,59 @@
     let header = document.getElementsByClassName('topnav')[0];
     let topnavBg = document.getElementById('topnavBg');
 
+    if (!portfoliocollapseRight) {
+        portfoliocollapseRight = document.createElement('button');
+        portfoliocollapseRight.classList.add('portfoliocollapse', 'right');
+        var p = document.createElement('p')
+        p.innerHTML = '&#9776';
+        portfoliocollapseRight.appendChild(p);
+        header.insertBefore(portfoliocollapseRight, portfoliocollapse);
+    }
+
+    let selButton = null;
+
 
     //Portfolio navbar collapsable button (mobile) 
     portfoliocollapse.addEventListener('click', function() {expandPortfolioNav()});
+    portfoliocollapseRight.addEventListener('click', function() {expandPortfolioNav()});
     
 
     function expandPortfolioNav() {
         //toggle active and change symbol for button
         portfoliocollapse.classList.toggle('active');
+        portfoliocollapseRight.classList.toggle('active');
+
         if (portfoliocollapse.classList.contains('active')) {
             portfoliocollapse.children[0].innerHTML = '&#9587';//'&#8855'; //'&#9932';
+            portfoliocollapseRight.children[0].innerHTML = '&#9587';//'&#8855'; //'&#9932';
         } else {
             portfoliocollapse.children[0].innerHTML = '&#9776';
+            portfoliocollapseRight.children[0].innerHTML = '&#9776';
         }
 
-        sidebarEffects();
+        var targetButton = event.target; //get the button, not 'p' or the topnav
+        if (targetButton.nodeName === 'P') {
+            targetButton = targetButton.parentElement;
+        }
+        selButton = targetButton;
+        sidebarEffects(targetButton); //send which button opened the menu
     }
 
-    function sidebarEffects() {
+    function sidebarEffects(e) {
+
+        console.log(portfoliocollapseRight.classList)
+
         //button effects on sidebar etc..
         topLogo.children[0].style.left = 'calc(50% - 1em)'; //center logo (säkerhets skull)
+
         if (sidebar.style.width) { // fäll in sidebar
             sidebar.style.width = null;
             topnavLinks.style.width = null;
             topLogo.children[0].style.top = '1.5em'; //'calc(50% - 1em)';
             //topLogo.style.top = '1.75em'; //'calc(50% - 1em)';
-            for (var i=0; i<icons.length; i++) {
+            /*for (var i=0; i<icons.length; i++) {
                 icons[i].style.width = '0';
-            }
+            }*/
             header.style.backgroundColor = 'rgba(0,0,0,0)';
             header.style.backgroundImage = 'linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0))'; //gradient to set empty bg image
             if (document.documentElement.scrollTop < 80) {
@@ -52,10 +78,10 @@
             //shift back body and header
             document.body.style.marginLeft = '0px';
             header.style.marginLeft = '0px';
+            document.body.style.marginRight = '0px';
+            header.style.marginRight = '0px';
             // remove icons
             const ic = document.getElementsByClassName('sidebarIcon');
-            console.log(ic)
-            const len = ic.length;
             for (let i=ic.length-1; i>=0; i--) {
                 ic[i].remove();
             }
@@ -65,13 +91,24 @@
             if (sidebar.children.length > 0) {
                 //Only pull out the sidebar if it has content
                 sidebar.style.width = '550px';
-                // and shift the document body & header
-                document.body.style.marginLeft = '550px';
-                header.style.marginLeft = '-550px';
+
+                if (e.classList.contains('right')) { //open the sidebar from the right
+                    sidebar.style.right = '0';
+                    sidebar.style.left = 'unset';
+                    // and shift the document body & header
+                    document.body.style.marginRight = '550px';
+                    header.style.marginRight = '-550px';
+                } else {    // or from the left
+                    sidebar.style.left = '0';
+                    sidebar.style.right = 'unset';
+                    // and shift the document body & header
+                    document.body.style.marginLeft = '550px';
+                    header.style.marginLeft = '-550px';
+                    }
             } else {
                 sidebar.style.width = '0px';
             }
-            topnavLinks.style.width = '46%'; //Kan behöva göra längre var vaksam
+            topnavLinks.style.width = 'calc(100% - 2 * 160px)'; //minus bredden på knapparna
             topLogo.children[0].style.top = '-100px';
             /*for (var i=0; i<icons.length; i++) {
                 icons[i].style.width = '1.5em';
@@ -116,8 +153,13 @@
             /*for (var i=0; i<icons.length; i++) {
                 icons[i].style.width = '0';
             }*/
-            sidebarEffects();
-            sidebarEffects();
+
+            var btn; //which way to close and reopen the sidebar on resize?
+            if (selButton) {
+                btn = selButton;
+            } else {btn = portfoliocollapse;}
+            sidebarEffects(btn);
+            sidebarEffects(btn);
         }
     }
 })();
