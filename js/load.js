0af6aca63b9f571715d8d88e925395ae2263f6ae
turpeluren <thenotteam@gmail.com>
2022-11-20 11:41:01 +0000

    fetch related resources

--- js/load.js
+++ js/load.js
@@ -31,17 +31,65 @@
 
     //related articles
 
-    // sidepatterns
-    var sPatterns = document.getElementById("related_vessel");
-    if (sPatterns) {
-        fetch('https://turpelurpeluren.online/resources/related/related_vessel.html') // the page we want to use for our sidepatterns
+    // vessel
+    if (document.getElementById("related_vessel")) {
+        fetch('https://turpelurpeluren.online/resources/related/related_vessel.html') 
         .then(data => {
             return data.text()
         })
         .then( data => {
-            sPatterns.innerHTML = data; // inserts to element id="sidepatterns"
+            sPatterns.innerHTML = data;
         })
     }
-
+    // boundless
+    if (document.getElementById("related_boundless")) {
+        fetch('https://turpelurpeluren.online/resources/related/related_boundless.html') 
+        .then(data => {
+            return data.text()
+        })
+        .then( data => {
+            sPatterns.innerHTML = data;
+        })
+    }
+    // table
+    if (document.getElementById("related_table")) {
+        fetch('https://turpelurpeluren.online/resources/related/related_table.html') 
+        .then(data => {
+            return data.text()
+        })
+        .then( data => {
+            sPatterns.innerHTML = data;
+        })
+    }
+    // pheasant
+    if (document.getElementById("related_pheasant")) {
+        fetch('https://turpelurpeluren.online/resources/related/related_pheasant.html') 
+        .then(data => {
+            return data.text()
+        })
+        .then( data => {
+            sPatterns.innerHTML = data;
+        })
+    }
+    // rightfully
+    if (document.getElementById("related_rightfully")) {
+        fetch('https://turpelurpeluren.online/resources/related/related_rightfully.html') 
+        .then(data => {
+            return data.text()
+        })
+        .then( data => {
+            sPatterns.innerHTML = data;
+        })
+    }
+    // soapholder
+    if (document.getElementById("related_soapholder")) {
+        fetch('https://turpelurpeluren.online/resources/related/related_soapholder.html') 
+        .then(data => {
+            return data.text()
+        })
+        .then( data => {
+            sPatterns.innerHTML = data;
+        })
+    }
 
 })();
