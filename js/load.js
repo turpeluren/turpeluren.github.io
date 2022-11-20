5bd917d14ba51ba422628e64e38384f767d5be01
turpeluren <thenotteam@gmail.com>
2022-11-20 11:34:17 +0000

    fetch related start

--- js/load.js
+++ js/load.js
@@ -1,40 +1,47 @@
 (function () {
     'use strict';
 
-    //window.onload = () => {
+    // footer
+    fetch('https://turpelurpeluren.online/resources/footer.html') // the page we want to use for our footer
+    .then(data => {
+        return data.text()
+    })
+    .then( data => {
+        document.getElementById("footer").innerHTML = data; // inserts to element id="footer"
+    })
+    // copy end
 
-        // footer
-        fetch('https://turpelurpeluren.online/resources/footer.html') // the page we want to use for our footer
+    // header
+    //  handled in sidebar.js
+
+    // sidepatterns
+    var sPatterns = document.getElementById("sidepatterns");
+    if (sPatterns) {
+        fetch('https://turpelurpeluren.online/resources/sidepatterns.html') // the page we want to use for our sidepatterns
         .then(data => {
             return data.text()
         })
         .then( data => {
-            document.getElementById("footer").innerHTML = data; // inserts to element id="footer"
+            sPatterns.innerHTML = data; // inserts to element id="sidepatterns"
         })
-        // copy end
+    }
 
-        // header
-        /*fetch('https://turpelurpeluren.online/resources/header.html') // the page we want to use for our header
+
+
+
+    //related articles
+
+    // sidepatterns
+    var sPatterns = document.getElementById("related_vessel");
+    if (sPatterns) {
+        fetch('https://turpelurpeluren.online/resources/related/related_vessel.html') // the page we want to use for our sidepatterns
         .then(data => {
             return data.text()
         })
         .then( data => {
-            document.getElementById("header").innerHTML = data; // inserts to element id="header"
-        })*/
-        // copy end
+            sPatterns.innerHTML = data; // inserts to element id="sidepatterns"
+        })
+    }
 
-        // sidepatterns
-        var sPatterns = document.getElementById("sidepatterns");
-        if (sPatterns) {
-            fetch('https://turpelurpeluren.online/resources/sidepatterns.html') // the page we want to use for our sidepatterns
-            .then(data => {
-                return data.text()
-            })
-            .then( data => {
-                sPatterns.innerHTML = data; // inserts to element id="sidepatterns"
-            })
-            // copy end
-        }
-    //}
 
 })();
