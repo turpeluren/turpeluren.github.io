0c45de0d45f55c30311c55b1be9800d23ebb5cf1
turpeluren <thenotteam@gmail.com>
2022-12-15 17:35:03 +0000

    functional dbild

--- js/dbild.js
+++ js/dbild.js
@@ -11,7 +11,84 @@ const jsondata = '{'+
 
         '},'+
         '"apr4": {'+
+            '"n10": "R68yNfU"'+
+        
+        '},'+
+        '"maj5": {'+
+
+        '},'+
+        '"jun6": {'+
             
+        '},'+
+        '"jul7": {'+
+
+        '},'+
+        '"aug8": {'+
+            
+        '},'+
+        '"sep9": {'+
+
+        '},'+
+        '"okt10": {'+
+            
+        '},'+
+        '"nov11": {'+
+
+        '},'+
+        '"dec12": {'+
+            
+        '}'+
+    '},'+
+    '"y2019": {'+
+        '"jan1": {'+
+
+        '},'+
+        '"feb2": {'+
+            
+        '},'+
+        '"mar3": {'+
+
+        '},'+
+        '"apr4": {'+
+        
+        '},'+
+        '"maj5": {'+
+
+        '},'+
+        '"jun6": {'+
+            
+        '},'+
+        '"jul7": {'+
+
+        '},'+
+        '"aug8": {'+
+            
+        '},'+
+        '"sep9": {'+
+
+        '},'+
+        '"okt10": {'+
+            
+        '},'+
+        '"nov11": {'+
+
+        '},'+
+        '"dec12": {'+
+            
+        '}'+
+    '},'+
+    '"y2020": {'+
+        '"jan1": {'+
+
+        '},'+
+        '"feb2": {'+
+            
+        '},'+
+        '"mar3": {'+
+
+        '},'+
+        '"apr4": {'+
+        
         '},'+
         '"maj5": {'+
 
@@ -96,18 +173,57 @@ const jsondata = '{'+
 
     playBtn.addEventListener('click', play);
     backBtn.addEventListener('click', stepBack);
+    forwBtn.addEventListener('click', stepForward);
+
+    var yy, mm, dd;
+    let size = 'l'; //m: minsta, l: low, h: high: tom string: största
+    var playTimeout;
+    var isPlaying = false;
 
     updateImage();
 
     function play() {
-        playIcon.classList.toggle('paused');
+
+        if (playIcon.classList.contains('paused')) {
+            playIcon.classList.remove('paused');
+            clearTimeout(playTimeout);
+            isPlaying = false;
+            size = 'l';
+            updateImage();
+        } else {
+            playIcon.classList.add('paused');
+            //playInterval = setInterval(playImages, 100);
+            gotoCorrectDayInMonth(1);
+            isPlaying = true;
+            size = 'm';
+        }
     }
+
+    function playImages() {
+        gotoCorrectDayInMonth(1);
+    }
+
+    function resumePlaying() {
+        isPlaying = true;
+        gotoCorrectDayInMonth(1);
+        console.log('resumes')
+    }
+    
+    img.addEventListener("load", event => {
+        var image = document.querySelector('img');
+        var isLoaded = image.complete && image.naturalHeight !== 0;
+        if (isPlaying) {
+            gotoCorrectDayInMonth(1);
+            isPlaying = false;
+            playTimeout = setTimeout(resumePlaying, 100);
+        }
+    });
 
     function updateImage() {
         // display image from the json object
         //  using the pointer array
         imgURL = eval("data."+pointer[0]+"."+pointer[1]+"."+pointer[2]);
-        img.src = "https://imgur.com/" + imgURL + "h.jpg";
+        img.src = "https://imgur.com/" + imgURL + size + ".jpg";
 
         //update date
         date.innerHTML = getDate();
@@ -147,39 +263,109 @@ const jsondata = '{'+
         return date;
     }
 
+    function stepForward() {
+        //go through the json
+        updateDate();
+
+        gotoCorrectDayInMonth(1);
+    }
+
     function stepBack() {
         //go through the json
-        var yy = parseInt(pointer[0].slice(1));
-        var mm = parseInt(pointer[1].slice(3));
-        var dd = parseInt(pointer[2].slice(1));
-        console.log(String(yy)+" "+String(mm)+" "+String(dd));
+        updateDate();
 
-        gotoCorrectDayInMonth();
-        
-        function gotoCorrectDayInMonth() {
-            // while previous day data does not exist & before 1st of the month
-            while (!eval("data."+pointer[0]+"."+pointer[1]+".n"+String(dd-1)) && dd > 1) {
-                // step back one day in the same month
-                pointer[2] = "n"+String(dd-1);
-                dd = dd - 1;
-            }
-            if (eval("data."+pointer[0]+"."+pointer[1]+".n"+String(dd-1))) {
-                //return if landed on a valid day
-                pointer[2] = "n"+String(dd-1);
-                console.log(pointer[2]);
-                updateImage()
+        gotoCorrectDayInMonth(-1);
+    }
+
+    function updateDate() {
+        yy = parseInt(pointer[0].slice(1));
+        mm = parseInt(pointer[1].slice(3));
+        dd = parseInt(pointer[2].slice(1));
+    }
+
+    function gotoCorrectDayInMonth(dir) {
+        yy = parseInt(pointer[0].slice(1));
+        mm = parseInt(pointer[1].slice(3));
+        dd = parseInt(pointer[2].slice(1));
+
+        // while previous day data does not exist & before 1st of the month
+        while (!eval("data."+pointer[0]+"."+pointer[1]+".n"+String(dd+dir)) && dd > -1 && dd < 33) {
+            // step back one day in the same month
+            pointer[2] = "n"+String(dd+dir);
+            dd = dd + dir;
+        }
+        if (eval("data."+pointer[0]+"."+pointer[1]+".n"+String(dd+dir))) {
+            //return if landed on a valid day
+            pointer[2] = "n"+String(dd+dir);
+            console.log(pointer[2]);
+            updateImage()
+            return
+        } else {
+            //else goto next month
+            gotoNextMonth(dir)
+            console.log("data."+pointer[0]+"."+pointer[1]+".n"+String(dd+dir));
+            gotoCorrectDayInMonth(dir)
+        }
+    }
+
+    //logs the month before the pointer
+    function gotoNextMonth(dir) {
+        if (mm == 1 && dir < 0) {
+            gotoNextYear(dir);
+        }
+        if (mm == 12 && dir > 0) {
+            gotoNextYear(dir);
+        }
+        for (key in Object.keys(data.y2021)) {
+            if (Object.keys(data.y2021)[key].includes(String(mm+dir))) {
+                //set month pointer
+                pointer[1] = Object.keys(data.y2021)[key];
+                //reset day pointer 
+                if (dir > 0) {
+                    pointer[2] = 'n0';
+                } else {
+                    pointer[2] = 'n32';
+                }
+                updateDate();
+                console.log(mm + " gotoNextMonth")
                 return
             }
         }
+    }
 
-        //logs the month before the pointer
-        for (key in Object.keys(data.y2021)) {
-            if (Object.keys(data.y2021)[key].includes(String(mm-1))) {
-                console.log(Object.keys(data.y2021)[key])
+    function gotoNextYear(dir) {
+        if (yy == 2018 && dir < 0) {
+            //loop to end
+            //reset all pointers
+            pointer[0] = 'y2022';
+            pointer[1] = 'dec13';
+            pointer[2] = 'n32';
+            updateDate();
+        }
+        if (yy == 2021 && dir > 0) {
+            //loop to start
+            //reset all pointers
+            pointer[0] = 'y2017';
+            pointer[1] = 'jan0';
+            pointer[2] = 'n0';
+            updateDate();
+        }
+        for (key in Object.keys(data)) {
+            if (Object.keys(data)[key].includes(String(yy+dir))) {
+                //set month pointer
+                pointer[0] = Object.keys(data)[key];
+                //reset day pointer 
+                if (dir > 0) {
+                    pointer[1] = 'jan0';
+                    pointer[2] = 'n0';
+                } else {
+                    pointer[1] = 'dec13';
+                    pointer[2] = 'n32';
+                }
+                updateDate();
+                return
             }
         }
-
-
     }
 
 })();
