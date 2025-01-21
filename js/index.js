643eed918fb5e6ac5de6518818d5db6a4f990d08
turpeluren <thenotteam@gmail.com>
2025-01-21 16:54:58 +0100

    one last try

--- js/index.js
+++ js/index.js
@@ -246,7 +246,7 @@
 	function sendMessage(name, info, message) {
 
 		var request = new XMLHttpRequest();
-		request.open("POST", process.env.DISCORD_WEBHOOK);
+		request.open("POST", import.meta.env.DISCORD_WEBHOOK);
 		request.setRequestHeader('Content-type', 'application/json');
 		// the below variables combine a label (text) + the textarea value + a newline (\n)
 		// add up to 2,000 characters (Discord's character limit)
@@ -265,7 +265,7 @@
 
     function subscribe(email, unsub=false) {
         var request = new XMLHttpRequest();
-		request.open("POST", process.env.DISCORD_WEBHOOK);
+		request.open("POST", import.meta.env.DISCORD_WEBHOOK);
 		request.setRequestHeader('Content-type', 'application/json');
 		// the below variables combine a label (text) + the textarea value + a newline (\n)
 		// add up to 2,000 characters (Discord's character limit)
